398b979f45800e07b8f6fe791a47f86b3e11530c
Jakub Ducho <87933202+Ghost9887@users.noreply.github.com>
2025-09-30 19:47:00 +0200

    Add files via upload

--- include/chunk.h
+++ include/chunk.h
@@ -3,4 +3,18 @@
 
 #include "common.h"
 
+typedef enum OpCode{
+  OP_RETURN
+}OpCode;
+
+//dynamic array
+typedef struct Chunk{
+  int count;
+  int capacity;
+  uint8_t *code;
+}Chunk;
+
+void initChunk(Chunk *chunk);
+void writeChunk(Chunk *chunk, uint8_t *byte);
+
 #endif
