bfde1ed7914b67a2199957525528ce436fb5239f
Jakub Ducho <87933202+Ghost9887@users.noreply.github.com>
2025-10-12 09:39:05 +0200

    Add files via upload

--- include/vm.h
+++ include/vm.h
@@ -5,6 +5,7 @@
 #include "common.h"
 #include "debug.h"
 #include "value.h"
+#include "compiler.h"
 
 #define STACK_MAX 256
 
@@ -22,7 +23,7 @@ typedef enum{
 }InterpretResult;
 
 void initVM();
-InterpretResult interpret(Chunk *chunk);
+InterpretResult interpret(const char *source);
 void push(Value value);
 Value pop();
 void freeVM();
